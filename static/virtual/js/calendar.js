347482c317b8f1a99c0a9a6cddadf7634af2db2d
Strobelt Hendrik <hendrik@strobelt.com>
2020-07-12 13:21:15 -0400

    test

--- static/virtual/js/calendar.js
+++ static/virtual/js/calendar.js
@@ -16,7 +16,7 @@ function make_cal(name, timezoneOffset) {
                 poster: '#94d222',
             },
             sunday_saturday: true,
-            min_date: "2020-07-15T04:45:00+00:00"
+            min_date: "2020-06-01T04:45:00+00:00"
         }
     }
 
@@ -136,6 +136,7 @@ function make_cal(name, timezoneOffset) {
                 },
             },
         });
+        console.log(Date.parse(min_date),"--- Date.parse(min_date)");
         calendar.setDate(Date.parse(min_date));
         calendar.createSchedules(events);
         calendar.on({
